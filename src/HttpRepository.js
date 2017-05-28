3b9da1ffabfedb8e9e85a9ebd8be1c27b3b7c9b1
Jeff Reznik <reznik.jeff@gmail.com>
2017-05-28 17:29:52 -0500

    added repositories

---
+++ src/HttpRepository.js
@@ -0,0 +1,67 @@
+import { http } from '@jeffreznik/utils'
+import HttpStatus from 'http-status-codes'
+import { AuthorizationError, HttpError, NotFoundError, ValidationError } from '@jeffreznik/error'
+
+class HttpRepository {
+  constructor(modelClass, endpoint) {
+    this.modelClass = modelClass
+    this.endpoint = endpoint
+  }
+
+  async find(attributes, raw = false) {
+    try {
+      const response = await http.get(this.endpoint, {params: attributes})
+      if (raw)
+        return response.data
+      else
+        return response.data.map(obj => new this.modelClass(obj, this))
+    } catch (error) {
+      if (error instanceof HttpError) {
+        if (error.status === HttpStatus.BAD_REQUEST) {
+          throw new ValidationError('invalid search criteria', error)
+        }
+        if (error.status === HttpStatus.UNAUTHORIZED) {
+          throw new AuthorizationError('unauthorized', error)
+        }
+      }
+      throw error.wrap(`unknown error retrieving ${this.modelClass.name} model(s) from repository`)
+    }
+  }
+
+  async save(model) {
+    try {
+      if (model.id) {
+        return await this.__update(model)
+      } else {
+        return await this.__create(model)
+      }
+    } catch (error) {
+      if (error instanceof HttpError) {
+        if (error.status === HttpStatus.BAD_REQUEST) {
+          // server will send over a validation error object in data
+          throw new ValidationError('property validation failed', error, error.data)
+        }
+        if (error.status === HttpStatus.NOT_FOUND) {
+          throw new NotFoundError('property not found', error)
+        }
+        if (error.status === HttpStatus.UNAUTHORIZED) {
+          throw new AuthorizationError('unauthorized', error)
+        }
+      }
+      const saveMethod = model.id ? 'updating' : 'creating'
+      throw error.wrap(`unknown error ${saveMethod} ${this.modelClass.name}`)
+    }
+  }
+
+  async __create(model) {
+    const response = await http.post(this.endpoint, model)
+    return new this.modelClass(response.data, this)
+  }
+
+  async __update(model) {
+    const response = await http.patch(`${this.endpoint}/${model.id}`, model)
+    return new this.modelClass(response.data, this)
+  }
+}
+
+export default HttpRepository
---
+++ src/MySqlRepository.js
@@ -0,0 +1,100 @@
+import { mysql } from '@jeffreznik/utils'
+import { GeneralError } from '@jeffreznik/error'
+
+class MySqlRepository {
+  constructor(modelClass, tableName) {
+    this.db = mysql.getConnection()
+    this.modelClass = modelClass
+    this.tableName = tableName
+  }
+
+  async find(attributes, raw = false) {
+    const sql = `select * from ${this.tableName} where ${this.__filterAttributes(attributes).join(' and ')}`
+    try {
+      const db = await this.db
+      const [results] = await db.query(sql, attributes)
+      return results.map(row => {
+        for (const attr in row) {
+          if (this.modelClass.defaultAttributes[attr] !== null && typeof this.modelClass.defaultAttributes[attr] === 'object') {
+            row[attr] = JSON.parse(row[attr])
+          }
+        }
+        if (raw)
+          return row
+        else
+          return new this.modelClass(row, this)
+      })
+    } catch (error) {
+      throw new GeneralError(`failed querying ${this.modelClass.name}`, error)
+    }
+  }
+
+  async findOne(attributes, raw = false) {
+    const results = await this.find(attributes, raw)
+    if (results.length) {
+      return results[0]
+    } else {
+      return false
+    }
+  }
+
+  async findById(id, raw = false) {
+    return await this.findOne({id}, raw)
+  }
+
+  __filterAttributes(attributes) {
+    const filtered = []
+    for (const key in attributes) {
+      if (this.modelClass.attributeNames.indexOf(key) !== -1)
+        filtered.push(`${key}=:${key}`)
+    }
+    return filtered
+  }
+
+  async save(model) {
+    try {
+      if (model.id) {
+        await this.__update(model)
+      } else {
+        model.id = await this.__insert(model) // mutating model here - maybe not the best idea
+      }
+      return model
+    } catch (error) {
+      const saveMethod = model.id ? 'updating' : 'inserting'
+      throw new GeneralError(`failed ${saveMethod} ${this.modelClass.name}`, error)
+    }
+  }
+
+  async __insert(model) {
+    const namedParameters = this.modelClass.writableAttributes.map(attr => ':' + attr)
+    const sql = `insert into ${this.tableName}
+      (${this.modelClass.writableAttributes.join()}) values
+      (${namedParameters.join()})`
+
+    const db = await this.db
+    const [result] = await db.query(sql, this.__convertValues(model))
+    return result.insertId
+  }
+
+  async __update(model) {
+    const updateFields = this.modelClass.writableAttributes.map(attr => `${attr}=:${attr}`)
+    const sql = `update ${this.tableName} set ${updateFields.join()} where id=:id`
+
+    const db = await this.db
+    const [result] = await db.query(sql, this.__convertValues(model))
+    return result.affectedRows
+  }
+
+  __convertValues(model) {
+    const values = {}
+    for (const attr in model) {
+      if (model[attr] !== null && typeof model[attr] === 'object')
+        values[attr] = JSON.stringify(model[attr])
+      else
+        values[attr] = model[attr]
+    }
+    return values
+  }
+}
+
+export default MySqlRepository
